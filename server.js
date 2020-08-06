c1c5efdb3d6c95f3035eef3885b644e7de713a3b
Ted Pedersen <tedpedersen@me.com>
2020-08-06 13:55:26 -0700

    post route with middleware

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { notes } = require("./db/db.json")
 
 function filterByQuery(query, notesArray) {
@@ -22,6 +26,11 @@ app.get("/api/notes", (req, res) => {
     results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 const PORT = process.env.PORT || 3001;
